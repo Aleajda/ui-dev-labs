83b284eb3372b514df18a51c8193a96b769b1978
Aleajda <alexus.fedorov@gmail.com>
2025-09-20 22:21:03 +0300

    fix: delete second index.html, delete some //

--- src/view/task-list-component.js
+++ src/view/task-list-component.js
@@ -27,39 +27,3 @@ export default class TaskListComponent {
         this.element = null;
     }
 }
-
-
-// import { createElement } from "../framework/render.js";
-// // import { tasks } from './../mock/task.js';
-
-// function createTaskComponentTemplate(task) {
-//     const {title} = task;
-    
-//     return `<div class="task">
-//                         ${title}
-//                     </div>`;
-// }
-
-// export default class TaskComponent {
-
-//     constructor({task}){
-//         this.task = task;
-//     }
-
-
-//     getTemplate() {
-//         return createTaskComponentTemplate(this.task);
-//     }
-
-//     getElement() {
-//         if (!this.element) {
-//             this.element = createElement(this.getTemplate());
-//         }
-
-//         return this.element;
-//     }
-
-//     removeElement() {
-//         this.element = null;
-//     }
-// }
